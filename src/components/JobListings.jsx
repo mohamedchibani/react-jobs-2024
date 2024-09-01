fac52d369d0c13caa55ba9e59f6e7d822761989e
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-09-01 19:35:02 +0100

    Loading Spinner & Conditional Fetching

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
@@ -7,8 +8,12 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome
+        ? "http://localhost:8000/jobs?_limit=3"
+        : "http://localhost:8000/jobs";
+
       try {
-        const res = await fetch("http://localhost:8000/jobs");
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
@@ -27,18 +32,19 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {/* <!-- Job Listing --> */}
-          {loading ? (
-            <h2>Loading...</h2>
-          ) : (
-            <>
-              {jobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-              ))}
-            </>
-          )}
-        </div>
+
+        {/* <!-- Job Listing --> */}
+        {loading ? (
+          <h2>
+            <Spinner loading={loading} />
+          </h2>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
