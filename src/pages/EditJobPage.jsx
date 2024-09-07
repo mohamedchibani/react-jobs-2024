e09d1c0aac60472bc5406f10d4973eab8c802462
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-09-07 14:06:03 +0100

    remove unused Link import

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 // import { useState, useEffect } from "react";
-import { useParams, useLoaderData, Link } from "react-router-dom";
+import { useParams, useLoaderData } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
